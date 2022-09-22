cb3e35c07e0b530b896f40c1e97a8b994a2efa1f
lfs <admin@guchengpengdeMacBook-Pro.local>
2022-09-23 07:36:59 +0800

    初始化工程

---
+++ Shared/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  Shared
+//
+//  Created by lfs on 2022/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
